d26cc0ba386826a933f4699f81e97a8a53c1f70a
dinglixiang <903975580@qq.com>
2013-10-24 14:23:06 +0800

    channel and blogroll model validation

--- app/models/blogroll.rb
+++ app/models/blogroll.rb
@@ -1,4 +1,5 @@
 class Blogroll < ActiveRecord::Base
   attr_accessible :title, :url
   validates_presence_of :title,:url
+  validates_format_of :url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix,message: "URL格式不正确"
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -4,4 +4,5 @@ class Channel < ActiveRecord::Base
   has_many :columns
 
   validates_presence_of :title,:link_str
+  validates_format_of :link_str, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix,message: "URL格式不正确"
 end
